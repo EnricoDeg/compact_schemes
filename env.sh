e47ec6af661c41617cabf154d00e1115a00f4d20
Enrico Degregori <degregori@dkrz.de>
2025-05-25 14:14:03 +0200

    env: update with stdc++ lib from nvhpc

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
 ml git nvhpc/23.9-gcc-11.2.0 openmpi/4.1.6-nvhpc-23.9
-export LD_LIBRARY_PATH=/sw/spack-levante/openmpi-4.1.6-mjsagq/lib
+export LD_LIBRARY_PATH=/sw/spack-levante/openmpi-4.1.6-mjsagq/lib:/home/k/k202136/cuAlgo/lib:/sw/spack-levante/gcc-11.2.0-bcn7mb/lib64
